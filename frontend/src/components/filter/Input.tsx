ce6e6f63bb3ab4cbe2167f910b95df549a2d9ec5
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-09-03 20:54:10 +0100

    fix: update input component styles for better dark mode support

--- frontend/src/components/filter/Input.tsx
+++ frontend/src/components/filter/Input.tsx
@@ -6,7 +6,7 @@ type InputProp = {
 const Input = ({placeHolder}: InputProp) => {
   return (
     <input className="outline-none w-full font-kumbh placeholder:text-dark-gray px-2 py-2
-    placeholder-shown:truncate" type="search" placeholder={placeHolder} />
+    placeholder-shown:truncate dark:text-gray-50" type="search" placeholder={placeHolder} />
   )
 }
 
