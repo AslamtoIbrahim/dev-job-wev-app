d0d12c02269b8b7de258d3d49c6544a9e65521a7
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-09-04 18:21:58 +0100

    feat: add load more component

---
+++ backend/data.ts
@@ -0,0 +1,8 @@
+export const description = 'Facilitates student understanding and application of concepts consistent with Idaho Content Standards through direct instruction, modeling, and encouragement of student participation in learning. Supports students in meeting academic and personal goals.\n\nGENERAL INFO\n\nTeacher - Secondary Music at Sandpoint High '
+
+export const items =  [
+"Possession of, or ability to obtain, Idaho Teaching Credential with endorsement for related subject area",
+"Ability to work effectively with students, staff, parents/guardians and other stakeholders",
+"Ability to utilize classroom instructional technology",
+"Applicants who do not currently hold an Idaho Administrator Credential are encouraged to contact the Idaho State Department of Education for certification requirements"
+]
--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,21 +1,25 @@
-import Filter from "../filter/Filter"
-import Header from "../head/Header"
-import JobCard from "./JobCard"
+import Filter from "../filter/Filter";
+import Header from "../head/Header";
+import JobCard from "./JobCard";
+import LoadButton from "./LoadButton";
 
 const Home = () => {
   return (
     <div>
-      <div className="back"/> 
+      <div className="back" />
       <Header />
       <Filter />
-      <div className="padx my-10 md:my-14 space-y-10 md:grid md:grid-cols-2 md:gap-x-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-x-4">
+      <div className="padx my-10 space-y-10 md:mt-14 md:mb-0 md:grid md:grid-cols-2 md:gap-x-2 lg:grid-cols-3 lg:gap-x-4 xl:grid-cols-4">
         <JobCard />
         <JobCard />
         <JobCard />
         <JobCard />
       </div>
+      <div className="flex w-full justify-center pb-8">
+        <LoadButton />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
