6b69d64fc9ce4483f8a73c25f0a9b6cd69fa4cea
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-11-10 15:40:12 +0100

    fix vercel deployment v1

--- backend/src/utils/databes.js
+++ backend/src/utils/databes.js
@@ -4,6 +4,9 @@ import path from "path";
 const dataPath = path.join(process.cwd(), "data.json");
 
 export const getJobDatatbase = () => {
+  console.log("🗂️ Current working directory:", process.cwd());
+  console.log("📄 Looking for file at:", dataPath);
+  console.log("📦 File exists:", fs.existsSync(dataPath));
   const data = fs.readFileSync(dataPath, 'utf-8')
   return JSON.parse(data)
 };
