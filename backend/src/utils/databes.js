b24065cfa8eac318c277c7a5acff61bfee89704b
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-11-10 15:48:21 +0100

    fix vercel deployment v2

--- backend/src/utils/databes.js
+++ backend/src/utils/databes.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const dataPath = path.join(process.cwd(), "data.json");
+const dataPath = path.join(process.cwd(), "backend", "data.json");
 
 export const getJobDatatbase = () => {
   console.log("🗂️ Current working directory:", process.cwd());
